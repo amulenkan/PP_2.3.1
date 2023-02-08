0eecf516385d53384d5b972a2bff7b6123f9473f
amulenkan <115099530+amulenkan@users.noreply.github.com>
2023-02-08 18:07:28 +0300

    поправила импорты

--- PP_2_3_1/src/main/java/crud/controller/UsersController.java
+++ PP_2_3_1/src/main/java/crud/controller/UsersController.java
@@ -5,8 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
 import crud.service.UserService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import javax.validation.Valid;
 
 
--- PP_2_3_1/src/main/java/crud/models/User.java
+++ PP_2_3_1/src/main/java/crud/models/User.java
@@ -3,7 +3,12 @@ package crud.models;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 
 
